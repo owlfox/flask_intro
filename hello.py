f20eb4a989f695b0a75f187e1ad9cefad4c03f26
TJC <tsejen.tw@gmail.com>
2016-08-12 11:33:27 +0800

    basic template example

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__) #python variable
 
 @app.route('/') #decorator, falsk takes the result back into the page
 def index():
-    return "<h1> Hello, CHPT!</h1>"
+    return render_template('index.html')
+
+@app.route('/user/<name>') 
+def user(name):
+    return render_template('user.html', name=name) 
 
 if __name__ == '__main__':
     app.run(debug=True)
